c8f392fcd305960c87424b1edf687576f62654b2
Nadhu <nadhu014@gmail.com>
2020-08-03 18:31:48 +0530

    Code Updated

---
+++ projects/zomato-app/App.js
@@ -0,0 +1,17 @@
+import { createStackNavigator } from 'react-navigation-stack';
+import { createAppContainer } from 'react-navigation';
+
+import SearchScreen from './src/screens/SearchScreen';
+import ResultsShowScreen from './src/screens/ResultsShowScreen';
+
+const navigator = createStackNavigator({
+  Search: SearchScreen,
+  ResultsShow: ResultsShowScreen
+}, {
+  initialRouteName: 'Search',
+  defaultNavigationOptions: {
+    title: 'Zomato App'
+  }
+});
+
+export default createAppContainer(navigator);
